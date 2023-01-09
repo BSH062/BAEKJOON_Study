2c37db051c17c6e78a236f63e6ad26f005c79ee2
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-09 12:50:43 +0900

    2023/01/09 백준 문제풀이 사칙연산 나눗셈

---
+++ BAEKJOON_05/C++.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    //C++은 cout 을 통한 출력은 6자리로 고정되기 때문에 강제로 자리수를 바꿔줘야함 문제가 9자리까지 요구 
+    cout << fixed;
+    cout.precision(9);
+    cout << a / b << endl;
+}
+
+
