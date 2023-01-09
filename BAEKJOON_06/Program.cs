b34a50cc219daddff137ba5cca7329cddf37fc60
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-09 13:13:42 +0900

    2023/01/09 백준 사칙연산 통합

---
+++ BAEKJOON_06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input =Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a%b);
+        }
+    }
+}
