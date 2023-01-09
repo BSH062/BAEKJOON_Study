1732d7d1f41f9e0287b9b6123f4208edadc778b1
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-09 11:51:33 +0900

    2023/01/09 백준 풀이 수정

--- BAEKJOON_03/C_Sharp.cs
+++ BAEKJOON_03/C_Sharp.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BAEKJOON_03
+namespace C_Sharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(input[0]) - int.Parse(input[1]));
         }
     }
 }
