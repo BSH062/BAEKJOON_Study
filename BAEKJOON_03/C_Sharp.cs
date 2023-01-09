430b71dabe419f8a271fbec811fff39c132abcbc
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-09 12:14:57 +0900

    2023/01/09 백준문제 풀이 C++추가

---
+++ BAEKJOON_03/C_Sharp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(input[0]) - int.Parse(input[1]));
+        }
+    }
+}
