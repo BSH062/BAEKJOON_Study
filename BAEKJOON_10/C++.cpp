48056dc69b3c89e13055d032c75a8735fbeb3327
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-10 18:03:40 +0900

    2023/01/10 백준 문제 풀이 나머지 구하기

---
+++ BAEKJOON_10/C++.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+#pragma warning(disable:4996)
+int main()
+{
+	int a, b, c ;
+	
+	scanf("%d %d %d", &a, &b, &c);
+
+	int result_a = (a + b) % c;
+	int result_b = ((a % c) + (b % c)) % c;
+	int result_c = (a * b) % c;
+	int result_d = ((a % c) * (b % c)) % c;
+
+	printf("%d\n%d\n%d\n%d\n", result_a, result_b, result_c, result_d);
+
+}
---
+++ BAEKJOON_10/C_Sharp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input= Console.ReadLine().Split();
+            int[] inputs = Array.ConvertAll(input, s => int.Parse(s));
+            int a = inputs[0], b = inputs[1], c = inputs[2];
+
+            int result_a = (a + b) % c;
+            int result_b = ((a % c) + (b % c)) % c;
+            int result_c = (a * b) % c;
+            int result_d = ((a % c) * (b % c)) % c;
+
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n", result_a, result_b, result_c, result_d);
+        }
+    }
+}
