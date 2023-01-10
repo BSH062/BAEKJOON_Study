3c09834af47483b22b3a5e35a830ce1b338cbb0e
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-10 16:30:10 +0900

    2023/01/10 백준문제풀이 불기 -서기 변환 (형변환)

---
+++ BAEKJOON_08/C++.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#pragma warning(disable:4996)
+
+
+
+int main()
+{
+	int year;
+	int BC = 2541 - 1998;
+
+	scanf("%d ", &year);
+
+	printf("%d \n", year-BC );
+
+}
---
+++ BAEKJOON_08/C_Sharp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int year = int.Parse(input);
+            int BCyear = 2541 - 1998;
+            int result = year - BCyear;
+
+            Console.WriteLine(result);
+        }
+    }
+}
