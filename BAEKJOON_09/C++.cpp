9450a3bf016b3665a53865f4dc962b1371c71706
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-10 17:07:40 +0900

    2023/01/10 백준 문제풀이 체스판 (배열, 반복문?)

---
+++ BAEKJOON_09/C++.cpp
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+#pragma warning(disable:4996)
+
+
+
+int main()
+{
+	////내가 현재 가진 체스 말의 수
+	//int a, b, c, d, e, f;
+
+	////체스 말의 원래 수 
+	//int king = 1;
+	//int Queen = 1;
+	//int Rppks = 2;
+	//int Bishops = 2;
+	//int Knigths = 2;
+	//int Pawns = 8;
+
+	//scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	//printf("%d %d %d %d %d %d \n", (king -a), ( Queen-b), ( Rppks-c), ( Bishops-d), ( Knigths-e), ( Pawns-f));
+
+	int chess[] = { 1,1,2,2,2,8 }, i, me;
+	for ( i = 0; i < 6; i++)
+	{
+		scanf("%d", &me);
+		printf("%d ", chess[i] - me);
+	}
+
+}
---
+++ BAEKJOON_09/C_Sharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] myChess = Array.ConvertAll(input,s=>int.Parse(s));
+            int[] chess = { 1, 1, 2, 2, 2, 8 };
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(chess[i] - myChess[i]+" ");
+            }
+        }
+    }
+}
